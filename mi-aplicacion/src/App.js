a4169f37c3b79e019bfa59521ecedcddb91829ff
Juan Camilo Cano Vargas <juan.cano@globant.com>
2017-11-20 18:04:45 -0300

    sdasd

--- mi-aplicacion/src/App.js
+++ mi-aplicacion/src/App.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './componentes/Estilos.css';
+import Start from './componentes/start-component';
+import View_Result from './componentes/view-results-component';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      estado:1
+    }
+  }
   render() {
     return (
-      <div className="App">
-
-       
-
       
+      <div className="App">
+        {
+          (
+            this.state.estado === 0
+            ?
+              <div>
+                  <section id="one"  className="sect-UI halo">
+                    <h1 className="title">
+                      WELCOME TO ADMIN CHALLENGES
+                    </h1>
+                  </section>
+                  <div align= "center" id="button">
+                            <button  type="button" class="btn btn-primary">VIEW CHALLENGES</button>
+                            <button  type="button" class="btn btn-primary">VIEW RESULTS CHALLENGES</button>
+                  </div>
+                  <Start>
+                  </Start>
+              </div>
+            :
+           ''
+          )
+        }
+        {
+          (
+            this.state.estado === 1
+            ?
+              <div style={{marginTop:60+'px'}}>
+                  <View_Result></View_Result>
+              </div>
+            :
+              ''
+          )
+        }
       </div>
     );
   }
---
+++ mi-aplicacion/src/componentes/start-component.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Start extends Component {
+
+    render() {
+
+        return (
+            <div>
+            
+        <table>
+                <tr>
+                  <th width="1000">CHALLENGE</th>
+                  <th>QUALIFICATION</th>
+                  <th>CV</th>
+                </tr>
+              </table>
+              </div>
+        );
+    }
+}
+export default Start;
